ff98a2cd25ec276d0f865d77e882d687d59a8dac
Reza Pahlevie <anlap4life@gmail.com>
2019-09-18 10:29:42 +0700

    commit CommonResponseModel

---
+++ src/main/java/com/example/demo/model/CommonResponseModel.java
@@ -0,0 +1,40 @@
+package com.example.demo.model;
+
+public class CommonResponseModel {
+    private String title;
+    private String code;
+    private String message;
+    private Object data;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+}
