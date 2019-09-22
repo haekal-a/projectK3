8cf5e1d8d012f9ee07494686ce754ab0cc85513a
haekal-a <amha20@outlook.com>
2019-09-22 22:06:49 +0700

    add approvedBy

--- src/main/java/com/example/demo/model/PersetujuanInputModel.java
+++ src/main/java/com/example/demo/model/PersetujuanInputModel.java
@@ -5,6 +5,7 @@ import java.math.BigDecimal;
 public class PersetujuanInputModel {
     private BigDecimal idPeminjaman;
     private String alasanPenolakan;
+    private String approvedBy;
 
     public BigDecimal getIdPeminjaman() {
         return idPeminjaman;
@@ -20,5 +21,13 @@ public class PersetujuanInputModel {
 
     public void setAlasanPenolakan(String alasanPenolakan) {
         this.alasanPenolakan = alasanPenolakan;
+    }
+
+    public String getApprovedBy() {
+        return approvedBy;
+    }
+
+    public void setApprovedBy(String approvedBy) {
+        this.approvedBy = approvedBy;
     }
 }
